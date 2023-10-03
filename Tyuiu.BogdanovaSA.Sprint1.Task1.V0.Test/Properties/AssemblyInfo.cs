a05d4b3d6beaa58b29b230c877cedb5ac893741d
tyuiu.bogdanovasnezhana@outlook.com <bogdanova.snezha09@gmail.com>
2023-10-03 21:31:25 +0500

    Task1

---
+++ Tyuiu.BogdanovaSA.Sprint1.Task1.V0.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.BogdanovaSA.Sprint1.Task1.V0.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.BogdanovaSA.Sprint1.Task1.V0.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("9390a627-7039-47ed-863b-ade61ade52a7")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Tyuiu.BogdanovaSA.Sprint1.Task1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.BogdanovaSA.Sprint1.Task1.V0.Lib;
+
+namespace Tyuiu.BogdanovaSA.Sprint1.Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #1 | Выполнила: Богданова С.А. | ИСПб-23-1";
+            Console.WriteLine("************************************************************************************************************");
+            Console.WriteLine("* Спринт #1                                                                                                *");
+            Console.WriteLine("* Тема: Базовые навыки работы в С#                                                                         *");
+            Console.WriteLine("* Задание #1                                                                                               *");
+            Console.WriteLine("* Вариант #16                                                                                              *");
+            Console.WriteLine("* Выполнила: Богданова Снежана Андреевна | ИСПб-23-1                                                       *");
+            Console.WriteLine("************************************************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                                                 *");
+            Console.WriteLine("* Написать программу, которая вычисляет выражение x*5*y+2*a                                                *");
+            Console.WriteLine("* И печатает результат на экране.                                                                          *");
+            Console.WriteLine("*                                                                                                          *");
+            Console.WriteLine("************************************************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                                                         *");
+            Console.WriteLine("************************************************************************************************************");
+            Console.WriteLine("* x*5*y+2*a                                                                                                *");
+            Console.WriteLine("************************************************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                                                               *");
+            Console.WriteLine("************************************************************************************************************");
+            double x, y, a;
+            Console.WriteLine(ds.Calculate());
+            Console.ReadLine();
+        }
+    }
+}
