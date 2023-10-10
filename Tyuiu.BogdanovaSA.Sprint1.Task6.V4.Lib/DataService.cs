f9652d0dee91392575f9907e062f1b1f01f04427
tyuiu.bogdanovasnezhana@outlook.com <bogdanova.snezha09@gmail.com>
2023-10-10 22:43:49 +0500

    Task6

---
+++ Tyuiu.BogdanovaSA.Sprint1.Task6.V4.Lib/DataService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.cources.programming.interfaces.Sprint1;
+
+namespace Tyuiu.BogdanovaSA.Sprint1.Task6.V4.Lib
+{
+    public class DataService : ISprint1Task6V4
+    {
+
+    }
+}
---
+++ Tyuiu.BogdanovaSA.Sprint1.Task6.V4.Test/DataServiceTest.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.BogdanovaSA.Sprint1.Task6.V4.Lib;
+
+namespace Tyuiu.BogdanovaSA.Sprint1.Task6.V4.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidString()
+        {
+            string strTest = "н * н";
+            DataServiceTest ds = new DataServiceTest();
+            string res = ds.WorkWithText(strTest);
+            string wait = "нн";
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
